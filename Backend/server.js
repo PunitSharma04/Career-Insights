2da9985b387e6377f28535065d59d9c39e042ce6
Punit Sharma <punitsharma191855101@gmail.com>
2026-06-04 17:20:10 +0530

    Add fallback port for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -2,11 +2,11 @@ require("dotenv").config()
 const connectToDB = require("./src/config/database")
 const app = require('./src/app');
 
-
+const PORT = process.env.PORT || 3000;
 
 connectToDB()
 
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
 })
